b7de68f627105f422a26af7695100c7d865f1650
R <admin@javastack.cn>
2022-12-28 17:34:03 +0800

    适配 Spring Boot 3.0.0

--- spring-boot-quartz/src/main/java/cn/javastack/springboot/quartz/TaskConfig.java
+++ spring-boot-quartz/src/main/java/cn/javastack/springboot/quartz/TaskConfig.java
@@ -3,7 +3,6 @@ package cn.javastack.springboot.quartz;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.quartz.*;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 
@@ -15,6 +14,7 @@ import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 @Configuration
 public class TaskConfig {
 
+    public static final String SIMPLE_TASK = "simple-task";
     private final SchedulerFactoryBean schedulerFactoryBean;
 
     /**
@@ -24,21 +24,24 @@ public class TaskConfig {
     @PostConstruct
     public void init() throws SchedulerException {
         Scheduler scheduler = schedulerFactoryBean.getScheduler();
-        scheduler.scheduleJob(simpleTask(), simpleTaskTrigger());
+        boolean exists = scheduler.checkExists(JobKey.jobKey(SIMPLE_TASK));
+        if (!exists) {
+            scheduler.scheduleJob(simpleTask(), simpleTaskTrigger());
+        }
     }
 
-//    @Bean
+    //    @Bean
     public JobDetail simpleTask() {
         return JobBuilder.newJob(SimpleTask.class)
-                .withIdentity("simple-task")
+                .withIdentity(SIMPLE_TASK)
                 .withDescription("简单任务")
                 .storeDurably()
                 .build();
     }
 
-//    @Bean
+    //    @Bean
     public Trigger simpleTaskTrigger() {
-        CronScheduleBuilder cronScheduleBuilder = CronScheduleBuilder.cronSchedule("0/3 * * * * ? *");
+        CronScheduleBuilder cronScheduleBuilder = CronScheduleBuilder.cronSchedule("0/5 * * * * ? *");
         return TriggerBuilder.newTrigger()
                 .withIdentity("simple-task-trigger")
                 .forJob(simpleTask())
