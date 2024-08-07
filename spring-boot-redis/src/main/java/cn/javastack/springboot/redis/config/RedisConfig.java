ebfa7be09c14a8697da6a82f272a89a50b0121dd
weipengzhen <weipengzhen@weipengzhendeMacBook-Pro.local>
2024-08-07 17:04:10 +0800

    redis部分增加了@Bean redisConnectionFactory

--- spring-boot-redis/src/main/java/cn/javastack/springboot/redis/config/RedisConfig.java
+++ spring-boot-redis/src/main/java/cn/javastack/springboot/redis/config/RedisConfig.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.*;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
@@ -18,6 +19,11 @@ import org.springframework.integration.redis.util.RedisLockRegistry;
  */
 @Configuration
 public class RedisConfig {
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory();
+    }
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
