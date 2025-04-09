8f23ac20a0f27758874468f53089e1275c2b065a
R <admin@javastack.cn>
2025-04-09 17:51:49 +0800

    Spring AI MCP

---
+++ spring-boot-ai/src/main/java/cn/javastack/springboot/ai/mcp/MCPClientController.java
@@ -0,0 +1,50 @@
+package cn.javastack.springboot.ai.mcp;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.model.function.FunctionCallback;
+import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * MCP 客户端控制器
+ * 微信公众号：Java技术栈
+ */
+@RequiredArgsConstructor
+@RestController
+@Slf4j
+public class MCPClientController {
+
+
+    private final ChatClient.Builder chatClientBuilder;
+
+    private final ToolCallbackProvider tools;
+
+    /**
+     * 打印出所有的 MCP 工具
+     * 微信公众号：Java技术栈
+     */
+    @PostConstruct
+    public void init() {
+        FunctionCallback[] toolCallbacks = tools.getToolCallbacks();
+        for (FunctionCallback toolCallback : toolCallbacks) {
+            log.debug("MCP Tool: {}", toolCallback.getName());
+        }
+    }
+
+    /**
+     * 网络搜索
+     * 微信公众号：Java技术栈
+     */
+    @GetMapping("/mcp/brave/search")
+    public String search(String input) {
+        var chatClient = chatClientBuilder
+                .defaultTools("spring_ai_mcp_client_brave_search_brave_web_search")
+                .build();
+        return chatClient.prompt(input).call().content();
+    }
+
+}
