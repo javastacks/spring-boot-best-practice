e22cafd4a577ec08bdfa584916dd3ff90a01ae73
R <admin@javastack.cn>
2025-07-01 15:33:03 +0800

    使用 Claude Code 代码生成

--- spring-boot-mybatis-plus/src/main/java/cn/javastack/springboot/mybatisplus/controller/UserController.java
+++ spring-boot-mybatis-plus/src/main/java/cn/javastack/springboot/mybatisplus/controller/UserController.java
@@ -1,35 +1,60 @@
 package cn.javastack.springboot.mybatisplus.controller;
 
+import java.time.LocalDateTime;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import cn.javastack.springboot.mybatisplus.entity.UserDO;
 import cn.javastack.springboot.mybatisplus.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 微信公众号：Java技术栈
  */
 @RequiredArgsConstructor
-@RestController
+@Controller
 public class UserController {
 
     private final UserService userService;
 
-    @GetMapping("/user/info/{id}")
-    public UserDO getUserInfoById(@PathVariable("id") long id) {
-        UserDO userDO = userService.getById(id);
-        return userDO;
+    @GetMapping("/add-user")
+    public String addUserPage() {
+        return "add-user";
     }
 
+    @GetMapping("/search-user")
+    public String searchUserPage() {
+        return "search-user";
+    }
 
-    @GetMapping("/user/info")
+    @ResponseBody
+    @GetMapping("/users/{id}")
+    public UserDO getUserInfoById(@PathVariable("id") long id) {
+        return userService.getById(id);
+    }
+
+    @ResponseBody
+    @GetMapping("/users")
     public UserDO getUserInfoByUsername(@RequestParam("username") String username,
                                         @RequestParam("type") int type) {
-        UserDO userDO = userService.getByUsername(username, type);
-        return userDO;
+        return userService.getByUsername(username, type);
     }
 
+    @ResponseBody
+    @PostMapping("/users")
+    public UserDO createUser(@RequestBody UserDO user) {
+        user.setCreateTime(LocalDateTime.now());
+        if (user.getStatus() == null) {
+            user.setStatus(1);
+        }
+        userService.save(user);
+        return user;
+    }
 
 }
--- spring-boot-mybatis-plus/src/main/java/cn/javastack/springboot/mybatisplus/entity/UserDO.java
+++ spring-boot-mybatis-plus/src/main/java/cn/javastack/springboot/mybatisplus/entity/UserDO.java
@@ -1,15 +1,20 @@
 package cn.javastack.springboot.mybatisplus.entity;
 
-import com.baomidou.mybatisplus.annotation.*;
-import lombok.Data;
-
 import java.io.Serializable;
 import java.time.LocalDateTime;
-import java.util.Date;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.FieldStrategy;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+
+import lombok.Data;
 
 
-@TableName("t_user")
 @Data
+@TableName("t_user")
 public class UserDO implements Serializable {
 
     @TableField()
