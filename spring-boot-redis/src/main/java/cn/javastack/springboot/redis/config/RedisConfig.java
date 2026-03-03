3e29df258fc131a1b225ec228c5c3d89628bb4f7
R <admin@javastack.cn>
2026-03-03 16:03:14 +0800

    升级并兼容 Spring Boot 4.0.3

--- spring-boot-redis/src/main/java/cn/javastack/springboot/redis/config/RedisConfig.java
+++ spring-boot-redis/src/main/java/cn/javastack/springboot/redis/config/RedisConfig.java
@@ -2,13 +2,11 @@ package cn.javastack.springboot.redis.config;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.*;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.integration.redis.util.RedisLockRegistry;
@@ -25,7 +23,7 @@ public class RedisConfig {
         template.setConnectionFactory(factory);
 
         StringRedisSerializer stringSerializer = new StringRedisSerializer();
-        RedisSerializer jacksonSerializer = getJacksonSerializer();
+        RedisSerializer<Object> jacksonSerializer = getJacksonSerializer();
 
         template.setKeySerializer(stringSerializer);
         template.setValueSerializer(jacksonSerializer);
@@ -37,12 +35,12 @@ public class RedisConfig {
         return template;
     }
 
-    private RedisSerializer getJacksonSerializer() {
-        ObjectMapper om = new ObjectMapper();
-        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-        om.activateDefaultTyping(LaissezFaireSubTypeValidator.instance,
-                ObjectMapper.DefaultTyping.NON_FINAL);
-        return new GenericJackson2JsonRedisSerializer(om);
+    private RedisSerializer<Object> getJacksonSerializer() {
+        return GenericJacksonJsonRedisSerializer.builder()
+                .customize(builder -> builder.changeDefaultVisibility(visibility ->
+                        visibility.withVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY)))
+                .enableUnsafeDefaultTyping()
+                .build();
     }
 
     @Bean
